58d9c9f703741a053915683038f2c97b087edceb
RaRhAeu <rarha_eu@protonmail.com>
2024-07-26 09:52:40 +0200

    fix: backport Optional[...]

--- pydantic_asyncapi/v2.py
+++ pydantic_asyncapi/v2.py
@@ -1,6 +1,4 @@
-from __future__ import annotations
-
-from typing import Annotated, Any, Literal, Union
+from typing import Annotated, Any, Literal, Optional, Union
 
 import annotated_types
 from pydantic import AnyUrl, Field
@@ -24,12 +22,12 @@ from .common import (
 
 
 class MessageTrait(BaseMessageTrait):
-    messageId: str | None = None
+    messageId: Optional[str] = None
 
 
 class Message(MessageTrait):
     payload: Any
-    traits: list[MessageTrait | Reference] | None = None
+    traits: Optional[list[Union[MessageTrait, Reference]]] = None
 
 
 MessageOrRef = Annotated[list[Union[Message, Reference]], annotated_types.Len(1, None)]
@@ -40,44 +38,46 @@ class OneOf(BaseModel):
 
 
 class OperationTrait(ExtendableBaseModel):
-    operationId: str | None = None
-    summary: str | None = None
-    description: str | None = None
-    security: list[SecurityScheme] | None = None
-    tags: list[Tag] | None = None
-    externalDocs: ExternalDocumentation | Reference | None = None
+    operationId: Optional[str] = None
+    summary: Optional[str] = None
+    description: Optional[str] = None
+    security: Optional[list[SecurityScheme]] = None
+    tags: Optional[list[Tag]] = None
+    externalDocs: Optional[Union[ExternalDocumentation, Reference]] = None
     bindings: TypeRefMap[Bindings] = None
 
 
 class Operation(OperationTrait):
-    traits: list[OperationTrait | Reference] | None = None
-    message: Message | Reference | OneOf
+    traits: Optional[list[Union[OperationTrait, Reference]]] = None
+    message: Union[Message, Reference, OneOf]
 
 
 class Parameter(BaseModel):
-    description: str | None = None
-    schema_: Schema | Reference | None = Field(None, alias="schema")
-    location: str | None = None
+    description: Optional[str] = None
+    schema_: Optional[Union[Schema, Reference]] = Field(None, alias="schema")
+    location: Optional[str] = None
 
 
 class ChannelItem(ExtendableBaseModel):
-    ref: str | None = Field(None, validation_alias="$ref", serialization_alias="$ref")
-    description: str | None = None
-    servers: list[str] | None = None
-    publish: Operation | None = None
-    subscribe: Operation | None = None
+    ref: Optional[str] = Field(
+        None, validation_alias="$ref", serialization_alias="$ref"
+    )
+    description: Optional[str] = None
+    servers: Optional[list[str]] = None
+    publish: Optional[Operation] = None
+    subscribe: Optional[Operation] = None
     parameters: TypeRefMap[Parameter] = None
-    bindings: Bindings | None = None
+    bindings: Optional[Bindings] = None
 
 
 class Server(ExtendableBaseModel):
     url: AnyUrl
     protocol: str
-    protocolVersion: str | None = None
-    description: str | None = None
+    protocolVersion: Optional[str] = None
+    description: Optional[str] = None
     variables: TypeRefMap[ServerVariable]
-    security: list[SecurityScheme] | None = None
-    tags: list[Tag] | None = None
+    security: Optional[list[SecurityScheme]] = None
+    tags: Optional[list[Tag]] = None
     bindings: TypeRefMap[Bindings] = None
 
 
@@ -97,18 +97,18 @@ class Components(BaseComponents):
 class Info(ExtendableBaseModel):
     title: str
     version: str
-    description: str | None = None
-    termsOfService: str | None = None
-    contact: Contact | None = None
-    license: License | None = None
+    description: Optional[str] = None
+    termsOfService: Optional[str] = None
+    contact: Optional[Contact] = None
+    license: Optional[License] = None
 
 
 class AsyncAPI(ExtendableBaseModel):
     asyncapi: Literal["2.6.0"] = "2.6.0"
-    id: str | None = None
+    id: Optional[str] = None
     info: Info
-    defaultContentType: str | None = None
+    defaultContentType: Optional[str] = None
     servers: TypeRefMap[Server] = None
     channels: dict[str, ChannelItem]
-    components: Components | None = None
-    tags: list[Tag] | None = None
+    components: Optional[Components] = None
+    tags: Optional[list[Tag]] = None
